75e4e6224f1e3d2c0a5d49ae567d569520119b9a
VInicius Gomes <vini.engmago@gmail.com>
2023-04-26 16:44:05 -0300

    mudança de visual

--- src/script.js
+++ src/script.js
@@ -44,9 +44,7 @@ function fecharModal() {
   document.getElementById("skillJs").checked = false;
   document.getElementById("skillBootstrap").checked = false;
 }
-function validarcpf(cpf) {
-  return cpf.length == 14;
-}
+
 function salvar() {
   let id = document.getElementById("id").value;
   let cpf = document.getElementById("cpf").value;
@@ -65,27 +63,18 @@ function salvar() {
   let skillBootstrap = document.getElementById("skillBootstrap").checked;
 
   if (!validarcpf(cpf)) {
-    return false == alert("erro");
+    return alert("erro");
   }
-
-  function validarcpf(cpf) {
-    if (typeof cpf !== "string") return false;
-    cpf = cpf.replace(/[^\d]+/g, "");
-    if (cpf.length !== 11 || !!cpf.match(/(\d)\1{10}/)) return false;
-    cpf = cpf.split("");
-    const validator = cpf
-      .filter((digit, index, array) => index >= array.length - 2 && digit)
-      .map((el) => +el);
-    const toValidate = (pop) =>
-      cpf
-        .filter((digit, index, array) => index < array.length - pop && digit)
-        .map((el) => +el);
-    const rest = (count, pop) =>
-      ((toValidate(pop).reduce((soma, el, i) => soma + el * (count - i), 0) *
-        10) %
-        11) %
-      10;
-    return !(rest(10, 2) !== validator[0] || rest(11, 1) !== validator[1]);
+  if (!nome.includes(" ")) {
+    return alert("nome invalido");
+  }
+  console.log(nascimento);
+  // var ano = nascimento.split("/")[2];
+  // var mes = nascimento.split("/")[1];
+  // var dia = nascimento.split("/")[0];
+  var [dia, mes, ano] = nascimento.split("/");
+  if (idade(ano, mes, dia) <= 16) {
+    return alert("idade invalida");
   }
 
   let candidato = {
@@ -141,6 +130,7 @@ function listarCandidatos() {
 
     // Funcionalidades botão editar
     let botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("btn-info", "btn-sm");
     let svg1 = `
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
@@ -154,6 +144,7 @@ function listarCandidatos() {
 
     // Funcionalidades botão remover
     let botaoRemover = document.createElement("button");
+    botaoRemover.classList.add("btn-danger", "btn-sm");
     let svg = `
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
   <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
@@ -248,3 +239,43 @@ const CpfMask = (value) => {
 
   return value;
 };
+
+function idade(ano_aniversario, mes_aniversario, dia_aniversario) {
+  var d = new Date(),
+    ano_atual = d.getFullYear(),
+    mes_atual = d.getMonth() + 1,
+    dia_atual = d.getDate(),
+    ano_aniversario = +ano_aniversario,
+    mes_aniversario = +mes_aniversario,
+    dia_aniversario = +dia_aniversario,
+    quantos_anos = ano_atual - ano_aniversario;
+
+  if (
+    mes_atual < mes_aniversario ||
+    (mes_atual == mes_aniversario && dia_atual < dia_aniversario)
+  ) {
+    quantos_anos--;
+  }
+
+  return quantos_anos < 0 ? 0 : quantos_anos;
+}
+
+function validarcpf(cpf) {
+  if (typeof cpf !== "string") return false;
+  cpf = cpf.replace(/[^\d]+/g, "");
+  if (cpf.length !== 11 || !!cpf.match(/(\d)\1{10}/)) return false;
+  cpf = cpf.split("");
+  const validator = cpf
+    .filter((digit, index, array) => index >= array.length - 2 && digit)
+    .map((el) => +el);
+  const toValidate = (pop) =>
+    cpf
+      .filter((digit, index, array) => index < array.length - pop && digit)
+      .map((el) => +el);
+  const rest = (count, pop) =>
+    ((toValidate(pop).reduce((soma, el, i) => soma + el * (count - i), 0) *
+      10) %
+      11) %
+    10;
+  return !(rest(10, 2) !== validator[0] || rest(11, 1) !== validator[1]);
+}
